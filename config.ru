283485fade08a814039d5221101bccd90781270d
flowerwrong <sysuyangkang@gmail.com>
2015-12-08 19:09:30 +0800

    Add config.ru

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './github_webhook'
+run Sinatra::Application
--- github_webhook.rb
+++ github_webhook.rb
@@ -4,6 +4,7 @@ require 'json'
 
 set :bind, '0.0.0.0'
 set :port, 8089
+set :server, 'thin'
 
 post '/push' do
   request.body.rewind
