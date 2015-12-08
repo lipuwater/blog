c66af9bf0e5300c8055fee3bb40bf2b97d6c5f13
flowerwrong <sysuyangkang@gmail.com>
2015-12-08 18:20:16 +0800

    Add auto update and github webhook

---
+++ github_webhook.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'json'
+
+set :bind, '0.0.0.0'
+set :port, 8089
+
+post '/push' do
+  request.body.rewind
+  payload_body = request.body.read
+  verify_signature(payload_body)
+  push = JSON.parse(payload_body)
+  puts "Received JSON: #{push.inspect}"
+  system('auto_update.sh') if 'refs/heads/master' == push['ref']
+end
+
+get '*' do
+  'What do you want? Nothing here...'
+end
+
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['GITHUB_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+end
