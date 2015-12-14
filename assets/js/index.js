3911391810d4bfd3082c0735c042c37d46673905
Bian Jiaping <ssbianjp@gmail.com>
2015-12-14 20:07:42 +0800

    Update clickable area of author

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+hljs.initHighlightingOnLoad();
+
+if (/team.html/.test(location.href)) {
+  document.body.onclick = function(event) {
+    var li;
+    var node = event.target;
+    while (node.parentNode) {
+      console.log(node);
+      if (node.tagName.toLowerCase() == 'li' && node.parentNode.className.indexOf('author-list') != -1) {
+        li = node;
+        break;
+      }
+      else {
+        node = node.parentNode;
+      }
+    }
+
+    if (li) {
+      location.href = li.querySelector('a').href;
+    }
+  }
+}
