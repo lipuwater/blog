7577a0b5761e3d7f55537ac473dea7236a1ca204
Bian Jiaping <ssbianjp@gmail.com>
2015-12-16 10:45:20 +0800

    Add mobile support

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,24 +1,37 @@
 //=require highlight.pack.js
 
-hljs.initHighlightingOnLoad();
+(function(){
+  hljs.initHighlightingOnLoad();
 
-if (/team.html/.test(location.href)) {
-  document.body.onclick = function(event) {
-    var li;
-    var node = event.target;
-    while (node.parentNode) {
-      console.log(node);
-      if (node.tagName.toLowerCase() == 'li' && node.parentNode.className.indexOf('author-list') != -1) {
-        li = node;
-        break;
+  if (/team.html/.test(location.href)) {
+    document.body.onclick = function(event) {
+      var li;
+      var node = event.target;
+      while (node.parentNode) {
+        console.log(node);
+        if (node.tagName.toLowerCase() == 'li' && node.parentNode.className.indexOf('author-list') != -1) {
+          li = node;
+          break;
+        }
+        else {
+          node = node.parentNode;
+        }
       }
-      else {
-        node = node.parentNode;
-      }
-    }
 
-    if (li) {
-      location.href = li.querySelector('a').href;
+      if (li) {
+        location.href = li.querySelector('a').href;
+      }
     }
   }
-}
+
+  var sidebar = document.querySelector('.sidebar');
+
+  document.querySelector('.sidebar-toggle').onclick = function(){
+    if (sidebar.className.indexOf('active') != -1) {
+      sidebar.className = 'sidebar';
+    }
+    else {
+      sidebar.className = 'sidebar active';
+    }
+  };
+})();
